b05b899fe77be811497d03e242260c46957cf759
Fractusa <56714875+Fractusa@users.noreply.github.com>
2026-03-27 23:02:10 +0100

    Changed camera angle to top down

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,8 +10,8 @@ public class Player : NetworkBehaviour
 
     public override void OnStartLocalPlayer()
     {
-        Camera.main.transform.SetParent(transform);
-        Camera.main.transform.localPosition = new Vector3(0,0,0);
+        // Camera.main.transform.SetParent(transform);
+        // Camera.main.transform.localPosition = new Vector3(0,0,0);
     }
 
     void Start()

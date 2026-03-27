ffb02cfb634fea03cf7a0110625a956dd24e57a7
Fractusa <56714875+Fractusa@users.noreply.github.com>
2026-03-27 22:40:14 +0100

    Mirror LAN functionality + Player Movement
    Added Mirror functionality allowing multiple clients to connect to the same game instance, currently only supports LAN. Also added movement through CharacterController + InputSystem

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Mirror;
+using UnityEngine.InputSystem;
+
+public class Player : NetworkBehaviour
+{
+    public float moveSpeed = 5f;
+    public InputAction moveAction;
+    private CharacterController controller;
+
+    public override void OnStartLocalPlayer()
+    {
+        Camera.main.transform.SetParent(transform);
+        Camera.main.transform.localPosition = new Vector3(0,0,0);
+    }
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+    
+    void Update()
+    {
+        if (!isLocalPlayer) {return;}
+
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+        Vector3 move = new Vector3(moveValue.x, 0, moveValue.y);
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}

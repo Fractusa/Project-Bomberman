934772b5e1f14f1bf010c3f5749e9aedd65da0ca
Fractusa <56714875+Fractusa@users.noreply.github.com>
2026-05-18 01:52:01 +0200

    Script cleanup
    Removed code meant for future use, will however likely not be used, deleted this for easier access. Can be setup again if deemed necessary.

--- Assets/Scripts/Lobby/RoomPlayer.cs
+++ Assets/Scripts/Lobby/RoomPlayer.cs
@@ -8,60 +8,13 @@ public class RoomPlayer : NetworkRoomPlayer
 {
     [SyncVar(hook = nameof(OnColorChoiceChanged))]
     public PlayerColorChoice colorChoice = PlayerColorChoice.Red;
-
-    [Header("UI")]
-    [SerializeField] private TMP_Dropdown colorDropdown;
-    [SerializeField] private TextMeshProUGUI selectedColorText;
-    [SerializeField] private Image selectedColorImage;
-
-    [Header("Lobby Preview")]
-    [SerializeField] private Renderer previewRenderer;
-    private bool isUpdatingUI;
-
-    private void OnColorChanged(Color oldColor, Color newColor)
-    {
-        Debug.Log($"Lobby color changed to {newColor}");
-    }
+    [SerializeField] private Renderer renderer;
 
     public override void OnStartClient()
     {
         base.OnStartClient();
 
-        SetupColorDropdown();
-        UpdateColorUI(colorChoice);
         ApplyLobbyPreviewColor(colorChoice);
-    }
-
-    private void SetupColorDropdown()
-    {
-        if (colorDropdown == null)
-            return;
-
-        colorDropdown.ClearOptions();
-        colorDropdown.AddOptions(
-            new System.Collections.Generic.List<string>(System.Enum.GetNames(typeof(PlayerColorChoice)))
-        );
-
-        colorDropdown.onValueChanged.RemoveListener(OnDropdownChanged);
-        colorDropdown.onValueChanged.AddListener(OnDropdownChanged);
-
-        colorDropdown.interactable = isLocalPlayer;
-    }
-
-    private void OnDropdownChanged(int index)
-    {
-        if(isUpdatingUI)
-            return;
-
-        if(!isLocalPlayer)
-            return;
-
-        PlayerColorChoice choice = (PlayerColorChoice)index;
-
-        CmdSetColorChoice(choice);
-
-        UpdateColorUI(choice);
-        ApplyLobbyPreviewColor(choice);
     }
 
     [Command]
@@ -75,33 +28,16 @@ public class RoomPlayer : NetworkRoomPlayer
 
     private void OnColorChoiceChanged(PlayerColorChoice oldChoice, PlayerColorChoice newChoice)
     {
-        UpdateColorUI(newChoice);
         ApplyLobbyPreviewColor(newChoice);
-    }
-
-    private void UpdateColorUI(PlayerColorChoice choice)
-    {
-        isUpdatingUI = true;
-
-        if (colorDropdown != null)
-            colorDropdown.value = (int)choice;
-
-        if (selectedColorText != null)
-            selectedColorText.text = $"Color: {choice}";
-
-        if (selectedColorImage != null)
-            selectedColorImage.color = PlayerColorUtility.ToColor(choice);
-
-        isUpdatingUI = false;
     }
 
     private void ApplyLobbyPreviewColor(PlayerColorChoice choice)
     {
-        if (previewRenderer == null)
-            previewRenderer = GetComponentInChildren<Renderer>();
+        if (renderer == null)
+            renderer = GetComponentInChildren<Renderer>();
 
-        if (previewRenderer != null)
-            previewRenderer.material.color = PlayerColorUtility.ToColor(choice);
+        if (renderer != null)
+            renderer.material.color = PlayerColorUtility.ToColor(choice);
     }
 
     public override void OnGUI()

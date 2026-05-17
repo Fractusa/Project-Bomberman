a0b0a58db9b7330881306f567600703be3030962
Fractusa <56714875+Fractusa@users.noreply.github.com>
2026-05-17 23:13:52 +0200

    Added the ability to choose player color in lobby
    The selected player color in lobby, should now recolor the player model to reflect the selected color. Should in theory be synced to other clients in the game, has however not been tested.

--- Assets/Scripts/Lobby/RoomPlayer.cs
+++ Assets/Scripts/Lobby/RoomPlayer.cs
@@ -1,107 +1,132 @@
 using UnityEngine;
 using Mirror;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor;
 
-/*
-	Documentation: https://mirror-networking.gitbook.io/docs/components/network-room-player
-	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkRoomPlayer.html
-*/
-
-/// <summary>
-/// This component works in conjunction with the NetworkRoomManager to make up the multiplayer room system.
-/// The RoomPrefab object of the NetworkRoomManager must have this component on it.
-/// This component holds basic room player data required for the room to function.
-/// Game specific data for room players can be put in other components on the RoomPrefab or in scripts derived from NetworkRoomPlayer.
-/// </summary>
 public class RoomPlayer : NetworkRoomPlayer
 {
-    #region Start & Stop Callbacks
+    [SyncVar(hook = nameof(OnColorChoiceChanged))]
+    public PlayerColorChoice colorChoice = PlayerColorChoice.Red;
 
-    /// <summary>
-    /// This is invoked for NetworkBehaviour objects when they become active on the server.
-    /// <para>This could be triggered by NetworkServer.Listen() for objects in the scene, or by NetworkServer.Spawn() for objects that are dynamically created.</para>
-    /// <para>This will be called for objects on a "host" as well as for object on a dedicated server.</para>
-    /// </summary>
-    public override void OnStartServer() { }
+    [Header("UI")]
+    [SerializeField] private TMP_Dropdown colorDropdown;
+    [SerializeField] private TextMeshProUGUI selectedColorText;
+    [SerializeField] private Image selectedColorImage;
 
-    /// <summary>
-    /// Invoked on the server when the object is unspawned
-    /// <para>Useful for saving object data in persistent storage</para>
-    /// </summary>
-    public override void OnStopServer() { }
+    [Header("Lobby Preview")]
+    [SerializeField] private Renderer previewRenderer;
+    private bool isUpdatingUI;
 
-    /// <summary>
-    /// Called on every NetworkBehaviour when it is activated on a client.
-    /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
-    /// </summary>
-    public override void OnStartClient() { }
+    private void OnColorChanged(Color oldColor, Color newColor)
+    {
+        Debug.Log($"Lobby color changed to {newColor}");
+    }
 
-    /// <summary>
-    /// This is invoked on clients when the server has caused this object to be destroyed.
-    /// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
-    /// </summary>
-    public override void OnStopClient() { }
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
 
-    /// <summary>
-    /// Called when the local player object has been set up.
-    /// <para>This happens after OnStartClient(), as it is triggered by an ownership message from the server. This is an appropriate place to activate components or functionality that should only be active for the local player, such as cameras and input.</para>
-    /// </summary>
-    public override void OnStartLocalPlayer() { }
+        SetupColorDropdown();
+        UpdateColorUI(colorChoice);
+        ApplyLobbyPreviewColor(colorChoice);
+    }
 
-    /// <summary>
-    /// This is invoked on behaviours that have authority, based on context and <see cref="NetworkIdentity.hasAuthority">NetworkIdentity.hasAuthority</see>.
-    /// <para>This is called after <see cref="OnStartServer">OnStartServer</see> and before <see cref="OnStartClient">OnStartClient.</see></para>
-    /// <para>When <see cref="NetworkIdentity.AssignClientAuthority"/> is called on the server, this will be called on the client that owns the object. When an object is spawned with <see cref="NetworkServer.Spawn">NetworkServer.Spawn</see> with a NetworkConnectionToClient parameter included, this will be called on the client that owns the object.</para>
-    /// </summary>
-    public override void OnStartAuthority() { }
+    private void SetupColorDropdown()
+    {
+        if (colorDropdown == null)
+            return;
 
-    /// <summary>
-    /// This is invoked on behaviours when authority is removed.
-    /// <para>When NetworkIdentity.RemoveClientAuthority is called on the server, this will be called on the client that owns the object.</para>
-    /// </summary>
-    public override void OnStopAuthority() { }
+        colorDropdown.ClearOptions();
+        colorDropdown.AddOptions(
+            new System.Collections.Generic.List<string>(System.Enum.GetNames(typeof(PlayerColorChoice)))
+        );
 
-    #endregion
+        colorDropdown.onValueChanged.RemoveListener(OnDropdownChanged);
+        colorDropdown.onValueChanged.AddListener(OnDropdownChanged);
 
-    #region Room Client Callbacks
+        colorDropdown.interactable = isLocalPlayer;
+    }
 
-    /// <summary>
-    /// This is a hook that is invoked on all player objects when entering the room.
-    /// <para>Note: isLocalPlayer is not guaranteed to be set until OnStartLocalPlayer is called.</para>
-    /// </summary>
-    public override void OnClientEnterRoom() { }
+    private void OnDropdownChanged(int index)
+    {
+        if(isUpdatingUI)
+            return;
 
-    /// <summary>
-    /// This is a hook that is invoked on all player objects when exiting the room.
-    /// </summary>
-    public override void OnClientExitRoom() { }
+        if(!isLocalPlayer)
+            return;
 
-    #endregion
+        PlayerColorChoice choice = (PlayerColorChoice)index;
 
-    #region SyncVar Hooks
+        CmdSetColorChoice(choice);
 
-    /// <summary>
-    /// This is a hook that is invoked on clients when the index changes.
-    /// </summary>
-    /// <param name="oldIndex">The old index value</param>
-    /// <param name="newIndex">The new index value</param>
-    public override void IndexChanged(int oldIndex, int newIndex) { }
+        UpdateColorUI(choice);
+        ApplyLobbyPreviewColor(choice);
+    }
 
-    /// <summary>
-    /// This is a hook that is invoked on clients when a RoomPlayer switches between ready or not ready.
-    /// <para>This function is called when the a client player calls SendReadyToBeginMessage() or SendNotReadyToBeginMessage().</para>
-    /// </summary>
-    /// <param name="oldReadyState">The old readyState value</param>
-    /// <param name="newReadyState">The new readyState value</param>
-    public override void ReadyStateChanged(bool oldReadyState, bool newReadyState) { }
+    [Command]
+    private void CmdSetColorChoice(PlayerColorChoice newChoice)
+    {
+        if (!System.Enum.IsDefined(typeof(PlayerColorChoice), newChoice))
+            return;
+        
+        colorChoice = newChoice;
+    }
 
-    #endregion
+    private void OnColorChoiceChanged(PlayerColorChoice oldChoice, PlayerColorChoice newChoice)
+    {
+        UpdateColorUI(newChoice);
+        ApplyLobbyPreviewColor(newChoice);
+    }
 
-    #region Optional UI
+    private void UpdateColorUI(PlayerColorChoice choice)
+    {
+        isUpdatingUI = true;
+
+        if (colorDropdown != null)
+            colorDropdown.value = (int)choice;
+
+        if (selectedColorText != null)
+            selectedColorText.text = $"Color: {choice}";
+
+        if (selectedColorImage != null)
+            selectedColorImage.color = PlayerColorUtility.ToColor(choice);
+
+        isUpdatingUI = false;
+    }
+
+    private void ApplyLobbyPreviewColor(PlayerColorChoice choice)
+    {
+        if (previewRenderer == null)
+            previewRenderer = GetComponentInChildren<Renderer>();
+
+        if (previewRenderer != null)
+            previewRenderer.material.color = PlayerColorUtility.ToColor(choice);
+    }
 
     public override void OnGUI()
     {
         base.OnGUI();
-    }
 
-    #endregion
+        if (!isLocalPlayer)
+            return;
+
+        GUILayout.BeginArea(new Rect(500, 20, 250, 300), "Player Color", GUI.skin.window);
+
+        GUILayout.Label($"Chosen color: {colorChoice}");
+
+        foreach (PlayerColorChoice choice in System.Enum.GetValues(typeof(PlayerColorChoice)))
+        {
+            GUI.backgroundColor = PlayerColorUtility.ToColor(choice);
+
+            if (GUILayout.Button(choice.ToString(), GUILayout.Height(35)))
+            {
+                CmdSetColorChoice(choice);
+            }
+        }
+
+        GUI.backgroundColor = Color.white;
+
+        GUILayout.EndArea();
+    }
 }
---
+++ Assets/Scripts/Player/PlayerColor.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Mirror;
+
+public class PlayerColor : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(OnColorChoiceChanged))]
+    public PlayerColorChoice colorChoice = PlayerColorChoice.Red;
+
+    [SerializeField] private Renderer targetRenderer;
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        ApplyColor(colorChoice);
+    }
+
+    private void OnColorChoiceChanged(PlayerColorChoice oldChoice, PlayerColorChoice newChoice)
+    {
+        ApplyColor(newChoice);
+    }
+
+    private void ApplyColor(PlayerColorChoice choice)
+    {
+        if (targetRenderer == null)
+            targetRenderer = GetComponentInChildren<Renderer>();
+
+        if (targetRenderer != null)
+            targetRenderer.material.color = PlayerColorUtility.ToColor(choice);
+    }
+
+}
